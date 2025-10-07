ed126ea01f02e1dc97c48507e7f9b985b828ca92
William Thibault <william.thibault@hiloenergie.com>
2025-10-06 20:22:10 -0400

    fix email

--- dev/back-end/cloud/netlify/functions/send-email.js
+++ dev/back-end/cloud/netlify/functions/send-email.js
@@ -61,7 +61,7 @@ exports.handler = async (event, context) => {
 
         // Create transporter using Proton Mail SMTP
         console.log('Creating Proton Mail transporter...');
-        const transporter = nodemailer.createTransporter({
+        const transporter = nodemailer.createTransport({
             host: 'mail.proton.me',
             port: 587,
             secure: false, // true for 465, false for other ports
